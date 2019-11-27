f6f55c023113ea299eef0869f53b13dada68e1f5
Juerg Ritter <jritter@redhat.com>
2019-11-27 22:59:41 +0100

    dynamic infopane works now

--- src/InfoPane.js
+++ src/InfoPane.js
@@ -1,33 +1,52 @@
 import React from 'react';
+import Flag from 'react-flags';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 //import $ from 'jquery';
 
 import './InfoPane.css';
 
 
 
-class InfoPane extends React.Component {
+function InfoPane(props) {
 
+  var flag = '';
+  if (props.post.country_code !== ''){
+    flag = <Flag name={props.post.country_code} format="png" pngSize="32" basePath="" />
+  }
 
-  render () {
-    return (
+  return (
+          <div className="modal-content" id="InfoPane">
+            <div className="modal-header d-flex justify-content-between">
+              <h5 className="modal-title">
+                <p dangerouslySetInnerHTML={{__html: props.post.title.rendered}}></p>
+              </h5>
+              <span>{flag}</span>
 
-
-            <div className="modal-content" id="InfoPane">
-              <div className="modal-header">
-                <h5 className="modal-title">Puebla und die Pyramide von Cholula</h5>
-              </div>
-              <div className="post-image">
-                <img src="https://itinerariodesconocido.ch/wp-content/uploads/2019/11/DSC04877.jpg" alt="" />
-              </div>
-              <div className="modal-body">
-                <p>Nach dem vielversprechenden Start in Mexico Stadt und dem nahen Teotihuacán ging es weiter in Richtung südosten, in Millionenstadt Puebla, der viertgrössten Stadt Mexikos. Für die Reise dorthin sind wir in einen öffentlichen Bus gestiegen, welcher uns in etwas mehr als 2 Stunden dorthin gebracht hat. Ich war positiv überrascht von der Qualität von diesen&#8230; <a className="more-link" href="https://itinerariodesconocido.ch/2019/11/17/puebla-und-die-pyramide-von-cholula/#more-2729" target="blank">weiterlesen &rarr;</a></p>
-              </div>
-              <div className="modal-footer">
-                <a className="btn btn-danger" href="https://itinerariodesconocido.ch/2019/11/17/puebla-und-die-pyramide-von-cholula/#more-2729" role="button" target="blank">Zum Blog Post</a>
+            </div>
+            <div className="post-image">
+              <img src={props.post.jetpack_featured_media_url} alt={props.post.title.rendered} />
+            </div>
+            <div className="modal-body">
+             <p dangerouslySetInnerHTML={{__html: props.post.excerpt.rendered}}></p>
+            </div>
+            <div className="modal-footer d-flex justify-content-between">
+              <a className="btn btn-danger" href={props.post.link} role="button" target="blank">Zum Blog Post</a>
+              <div className="btn-group" role="group" aria-label="...">
+                <button type="button" className="btn btn-secondary" onClick={props.prev}><FontAwesomeIcon icon={faChevronLeft} /></button>
+                <button type="button" className="btn btn-secondary" onClick={props.next}><FontAwesomeIcon icon={faChevronRight} /></button>
               </div>
             </div>
-    );
-  }
+          </div>
+  );
+
 }
 
 export default InfoPane;
+
+// 
+// 
+// {props.post.title.rendered}
+// 
+// 
--- src/Map.js
+++ src/Map.js
@@ -7,13 +7,23 @@ import InfoPane from './InfoPane';
 import './Map.css';
 import 'leaflet/dist/leaflet.css';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-regular-svg-icons';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { faCircle } from '@fortawesome/free-regular-svg-icons';
 
-import { renderToString } from 'react-dom/server'
+//import { renderToString } from 'react-dom/server'
 
 
 class Map extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    var map = this;
+    this.state = { posts: null, current: 1 };
+    $.getJSON('/itinerario_desconocido_posts.json', function(data){
+      map.setState({ posts: data });
+    });
+  }
 
   componentDidMount() {
     var map = L.map('map').setView([51.505, -0.09], 5);
@@ -25,27 +35,42 @@ class Map extends React.Component {
       ext: 'png',
     }).addTo(map);
     
-    var myIcon = L.divIcon({className: 'map-marker', html: renderToString(<FontAwesomeIcon icon={faCircle} />)});
+    //var myIcon = L.divIcon({className: 'map-marker', html: renderToString(<FontAwesomeIcon icon={faCircle} />)});
 
-    $.getJSON('/places_i_wanna_go.geojson', function(data){
-      L.geoJSON(data, { pointToLayer: function (feature,latLng) {
-        return L.marker(latLng,{icon: myIcon})
-      }}).addTo(map);
-    });
+    
+    
+  }
 
-    $.getJSON('/itinerario_desconocido_posts.json', function(data){
-      console.log(data[0].country_code);
-    });
+  next() {
+    var num_posts = this.state.posts.length;
+    var current = this.state.current;
+    this.setState({ current : mod(current + 1, num_posts)});
+  }
+
+  prev() {
+    var num_posts = this.state.posts.length;
+    var current = this.state.current;
+    this.setState({ current : mod(current - 1, num_posts)});
   }
 
   render () {
+    var pane = '';
+
+    if (this.state.posts !== null) {
+      pane = <InfoPane post={this.state.posts[this.state.current]} prev={() => this.prev()} next={() => this.next()}/>          
+    }
+    
     return (
           <div>
             <div id="map"></div>
-            <InfoPane />
+            {pane}
           </div>
     );
   }
 }
 
 export default Map;
+
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
