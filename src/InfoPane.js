a63f6141f9b4f4546fffd4dcf1edeba4b11068f0
Juerg Ritter <jritter@redhat.com>
2019-12-04 20:33:23 +0100

    run prettier

--- src/InfoPane.js
+++ src/InfoPane.js
@@ -1,52 +1,80 @@
 import React from 'react';
 import Flag from 'react-flags';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 //import $ from 'jquery';
 
 import './InfoPane.css';
 
-
-
 function InfoPane(props) {
-
   var flag = '';
-  if (props.post.country_code !== ''){
-    flag = <Flag name={props.post.country_code} format="png" pngSize="32" basePath="" />
+  if (props.post.country_code !== '') {
+    flag = (
+      <Flag
+        name={props.post.country_code}
+        format="png"
+        pngSize="32"
+        basePath=""
+      />
+    );
   }
 
   return (
-          <div className="modal-content" id="InfoPane">
-            <div className="modal-header d-flex justify-content-between">
-              <h5 className="modal-title">
-                <p dangerouslySetInnerHTML={{__html: props.post.title.rendered}}></p>
-              </h5>
-              <span>{flag}</span>
-
-            </div>
-            <div className="post-image">
-              <img src={props.post.jetpack_featured_media_url} alt={props.post.title.rendered} />
-            </div>
-            <div className="modal-body">
-             <p dangerouslySetInnerHTML={{__html: props.post.excerpt.rendered}}></p>
-            </div>
-            <div className="modal-footer d-flex justify-content-between">
-              <a className="btn btn-danger" href={props.post.link} role="button" target="blank">Zum Blog Post</a>
-              <div className="btn-group" role="group" aria-label="...">
-                <button type="button" className="btn btn-secondary" onClick={props.prev}><FontAwesomeIcon icon={faChevronLeft} /></button>
-                <button type="button" className="btn btn-secondary" onClick={props.next}><FontAwesomeIcon icon={faChevronRight} /></button>
-              </div>
-            </div>
-          </div>
+    <div className="modal-content" id="InfoPane">
+      <div className="modal-header d-flex justify-content-between">
+        <h5 className="modal-title">
+          <p
+            dangerouslySetInnerHTML={{ __html: props.post.title.rendered }}
+          ></p>
+        </h5>
+        <span>{flag}</span>
+      </div>
+      <div className="post-image">
+        <img
+          src={props.post.jetpack_featured_media_url}
+          alt={props.post.title.rendered}
+        />
+      </div>
+      <div className="modal-body">
+        <p
+          dangerouslySetInnerHTML={{ __html: props.post.excerpt.rendered }}
+        ></p>
+      </div>
+      <div className="modal-footer d-flex justify-content-between">
+        <a
+          className="btn btn-danger"
+          href={props.post.link}
+          role="button"
+          target="blank"
+        >
+          Zum Blog Post
+        </a>
+        <div className="btn-group" role="group" aria-label="...">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={props.prev}
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={props.next}
+          >
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </div>
+      </div>
+    </div>
   );
-
 }
 
 export default InfoPane;
 
-// 
-// 
+//
+//
 // {props.post.title.rendered}
-// 
-// 
+//
+//
--- src/Map.js
+++ src/Map.js
@@ -19,26 +19,26 @@ class Map extends React.Component {
     var blogmap = this;
 
     $(document).keydown(function(e) {
-      switch(e.which) {
-          case 37: // left
+      switch (e.which) {
+        case 37: // left
           blogmap.prev();
           break;
 
-          case 38: // up
+        case 38: // up
           break;
 
-          case 39: // right
+        case 39: // right
           blogmap.next();
           break;
 
-          case 40: // down
+        case 40: // down
           break;
 
-          default: return; // exit this handler for other keys
+        default:
+          return; // exit this handler for other keys
       }
       e.preventDefault(); // prevent the default action (scroll / move caret)
     });
-
 
     this.state = { posts: null, current: 1 };
     $.when(
@@ -48,14 +48,11 @@ class Map extends React.Component {
     ).then(function() {
       blogmap.moveToPost(blogmap.state.posts.length - 1);
     });
-
-
-    
   }
 
   componentDidMount() {
     var map = L.map("map").setView([51.505, -0.09], 5);
-    map.setActiveArea('activeArea');
+    map.setActiveArea("activeArea");
     L.tileLayer(
       "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}",
       {
@@ -67,16 +64,16 @@ class Map extends React.Component {
         ext: "png"
       }
     ).addTo(map);
-    
+
     const pulsemarkersvg = L.icon({
-      iconUrl: '/marker-pulse.svg',
-      iconSize: [80, 80],
+      iconUrl: "/marker-pulse.svg",
+      iconSize: [80, 80]
     });
 
-    const pulsemarker = L.marker([0, 0], {icon: pulsemarkersvg});
+    const pulsemarker = L.marker([0, 0], { icon: pulsemarkersvg });
     pulsemarker.addTo(map);
-    
-    this.setState({ map: map, pulsemarker: pulsemarker});
+
+    this.setState({ map: map, pulsemarker: pulsemarker });
 
     //var myIcon = L.divIcon({className: 'map-marker', html: renderToString(<FontAwesomeIcon icon={faCircle} />)});
   }
@@ -98,40 +95,42 @@ class Map extends React.Component {
     var blogmap = this;
 
     if (coords !== "") {
-      $.getJSON("/ne/countries/" + this.state.posts[i].country_code + ".json", function(data) {
-        
-        if (blogmap.state.countrylayer != null){
-          blogmap.state.map.removeLayer(blogmap.state.countrylayer);
-        }
-
-        const countrylayer = L.geoJSON(data, {
-          style: function (feature) {
-            return {
-              color: '#bc4747',
-              fillOpacity: 0.2,
-              opacity: 0.5,
-              weight: 5
-            };
+      $.getJSON(
+        "/ne/countries/" + this.state.posts[i].country_code + ".json",
+        function(data) {
+          if (blogmap.state.countrylayer != null) {
+            blogmap.state.map.removeLayer(blogmap.state.countrylayer);
           }
-        })
 
-        countrylayer.addTo(blogmap.state.map);
-        
-        blogmap.setState({ countrylayer: countrylayer });
-        blogmap.state.map.flyToBounds(countrylayer.getBounds());
+          const countrylayer = L.geoJSON(data, {
+            style: function(feature) {
+              return {
+                color: "#bc4747",
+                fillOpacity: 0.2,
+                opacity: 0.5,
+                weight: 5
+              };
+            }
+          });
 
-        try {
-          const position = L.latLng(coords.split(" ").map(x => parseFloat(x)));
-          blogmap.state.pulsemarker.setLatLng(position);
-        } catch (error) {
-          console.log("error reading coordinates: " + error);
+          countrylayer.addTo(blogmap.state.map);
+
+          blogmap.setState({ countrylayer: countrylayer });
+          blogmap.state.map.flyToBounds(countrylayer.getBounds());
+
+          try {
+            const position = L.latLng(
+              coords.split(" ").map(x => parseFloat(x))
+            );
+            blogmap.state.pulsemarker.setLatLng(position);
+          } catch (error) {
+            console.log("error reading coordinates: " + error);
+          }
         }
-        
-      })
-      
+      );
     }
-    
-    this.setState({ current: i});
+
+    this.setState({ current: i });
   }
 
   render() {
