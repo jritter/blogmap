156789d1d96a9934bf4c468c9d979fba05ee350d
Juerg Ritter <jritter@redhat.com>
2024-03-18 22:40:23 +0100

    linting

--- src/InfoPane.js
+++ src/InfoPane.js
@@ -25,38 +25,34 @@ function InfoPane(props) {
       <div className="post-header justify-content-between">
         <h1
           className="post-title entry-title"
-          dangerouslySetInnerHTML={{ __html: props.post.title.rendered }}
+          dangerouslySetInnerHTML={{__html: props.post.title.rendered}}
         ></h1>
       </div>
       <div className="featured-media">
         <img src={props.image.src} alt={props.image.alt} />
       </div>
       <div className="post-content clear">
-        <p
-          dangerouslySetInnerHTML={{ __html: props.post.excerpt.rendered }}
-        ></p>
+        <p dangerouslySetInnerHTML={{__html: props.post.excerpt.rendered}}></p>
       </div>
       <div className="post-footer">
         <div>
           {flag}
-          <span style={{ float: 'right' }}>
-            <a className="blogmap-button" href={props.post.link} target="blank">
+          <span style={{float: 'right'}}>
+            <button className="blogmap-button" href={props.post.link} target="blank">
               Zum Blog Post
-            </a>
-            <a
+            </button>
+            <button
               className="blogmap-button blogmap-arrow-button"
               onClick={props.prev}
-              href="#"
             >
               <FontAwesomeIcon icon={faChevronLeft} />
-            </a>
-            <a
+            </button>
+            <button
               className="blogmap-button blogmap-arrow-button"
               onClick={props.next}
-              href="#"
             >
               <FontAwesomeIcon icon={faChevronRight} />
-            </a>
+            </button>
           </span>
         </div>
       </div>
