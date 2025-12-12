c7d9523798f9a195e37a08e58c16f92054fe5062
Juerg Ritter <jritter@redhat.com>
2025-12-12 16:24:29 +0100

    chore: Bump library versions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import Map from './Map';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Map />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<Map />);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
